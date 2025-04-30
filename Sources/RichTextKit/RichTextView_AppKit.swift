2d4e99ae48098000ab015bdd1bf07211c36a7544
Rizwana <rizwana@simformsolutions.com>
2025-04-30 22:29:40 +0530

    Remove commented/unused code

--- Sources/RichTextKit/RichTextView_AppKit.swift
+++ Sources/RichTextKit/RichTextView_AppKit.swift
@@ -1016,16 +1016,6 @@ open class RichTextView: NSTextView, RichTextViewComponent {
         DispatchQueue.main.async{
             self.scrollRangeToVisible(range)
         }
-
-        // Ensure we scroll to the bottom by scrolling a bit more
-//        if let scrollView = enclosingScrollView {
-//            let visibleRect = scrollView.documentVisibleRect
-//            let contentRect = scrollView.documentView?.frame ?? .zero
-//            let bottomOffset = contentRect.maxY - visibleRect.maxY
-//            if bottomOffset > 0 {
-//                scrollView.contentView.scroll(to: NSPoint(x: 0, y: contentRect.maxY - visibleRect.height))
-//            }
-//        }
     }
 
     /// Set the rich text in the text view.
