8e3cb347a82a323e07c3f73f59faeb8b7ab260aa
Rizwana <rizwana@simformsolutions.com>
2025-08-13 20:26:45 +0530

    Add Open Note call back for iOS

--- Sources/RichTextKit/RichTextEditor.swift
+++ Sources/RichTextKit/RichTextEditor.swift
@@ -207,17 +207,13 @@ public extension RichTextEditor {
 
     public func openNote(_ completion: @escaping (String) -> ()) -> RichTextEditor {
         let editor = self
-#if os(macOS)
         editor.textView.openNote = completion
-#endif
-        // TODO: Other OS call back
         return editor
     }
 
     public func openSection(_ completion: @escaping (String) -> ()) -> RichTextEditor {
         let editor = self
         editor.textView.openSection = completion
-        // TODO: Other OS call back
         return editor
     }
 
